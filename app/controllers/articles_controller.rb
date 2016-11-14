219f54cd3e2ff722df25bbc48dd3c0cb972170ca
Jean Aguilar <jean@MacBook-Pro-de-Jean.local>
2016-11-14 04:26:37 -0600

    Asociaciones 1 a muchos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
 	end
 	#POST /articles
 	def create
-		@article = Article.new(article_params)
+		@article = current_user.articles.new(article_params)
 
 		if @article.save
 			redirect_to @article
