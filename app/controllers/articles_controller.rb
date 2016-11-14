8053d0e5a9a1535d4d5c0bf144f614a7d960d3e6
Jean Aguilar <jean@MacBook-Pro-de-Jean.local>
2016-11-14 02:05:39 -0600

    Vista parcial form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,25 @@
 class ArticlesController < ApplicationController
 	#GET /articles
 	def index
+		#OTodos los registros
 		@articles = Article.all
 	end
 	#GET /articles/:id
 	def show
+		#Buscar registros en este caso por su id
 		@article = Article.find(params[:id])
 	end
 	#GET /articles/new
 	def new
 		@article = Article.new
 	end
+	#
+	def edit
+		@article = Article.find(params[:id])
+	end
 	#POST /articles
 	def create
-		@article = Article.new(title: params[:article][:title],
-								body: params[:article][:body])
+		@article = Article.new(article_params)
 
 		if @article.save
 			redirect_to @article
@@ -22,5 +27,27 @@ class ArticlesController < ApplicationController
 			render :new				
 		end
 		
+	end
+	#DELETE /articles/:id
+	def destroy	
+		@article = Article.find(params[:id])
+		@article.destroy #Destroy elimina el objeto en la base de datos
+		redirect_to articles_path
+	end
+	#PUT /articles/:id
+	def update
+		# @article.update_attributes({title: 'Nuevo título'})
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit	
+		end
+	end
+	#Metodos privados
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :body)
 	end
 end
